2b657ae13dff76a03bd4384a5a5f30669d5d038c
Bright Borngreat <borngreatbright019@gmail.com>
2026-01-18 08:15:45 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'currencyflow-v1';
+const assets = [
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './logo-192x192.png',
+  './logo-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Poppins:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
